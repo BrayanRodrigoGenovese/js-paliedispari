a45c5431f4875d12930dbf8121c5e753ea38c5e7
Brayan <bittolo.8bit@gmail.com>
2026-01-08 18:00:13 +0100

    Refactor random number generation and user input handling

--- odds-and-even/script.js
+++ odds-and-even/script.js
@@ -6,4 +6,18 @@ function isEven(number) {
   return number % 2 === 0;
 }
 
-console.log(randomNumber(1, 5));
+const computerNumber = randomNumber(1, 5);
+const userGuess = prompt("Pari o Dispari?").trim().toLowerCase();
+const userNumber = parseInt(prompt("inserisci un numero da 1 a 5"));
+const even = isEven(userNumber + computerNumber);
+
+console.log(userGuess);
+console.log(computerNumber);
+console.log(userNumber);
+console.log(computerNumber + userNumber);
+
+if ((userGuess === "pari") === even) {
+  console.log("hai vinto");
+} else {
+  console.log("hai perso");
+}
