2b7a08429a5ed0c33a0339021e5d114f3dc4066b
Brayan <bittolo.8bit@gmail.com>
2026-01-08 17:36:50 +0100

    adding isEven function

--- odds-and-even/script.js
+++ odds-and-even/script.js
@@ -1,4 +1,9 @@
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
 console.log(randomNumber(1, 5));
