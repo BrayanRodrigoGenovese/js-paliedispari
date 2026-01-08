df942fc81832f257b919d96ea3f662d44d329b45
Brayan <bittolo.8bit@gmail.com>
2026-01-08 16:15:05 +0100

    Implement palindrome verification logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+const userWord = prompt("Inserisci la parola per verificare se è palindroma");
+function isPalindrome(word) {
+  reversedUserWord = "";
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    reversedUserWord += userWord[i];
+  }
+  return userWord === reversedUserWord;
+}
 
+console.log(isPalindrome(userWord));
